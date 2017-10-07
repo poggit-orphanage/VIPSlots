c3faaa19c4c7ff560c85dbb5965bc20b39da1474
jazzwhistle <jdcraftmcpe@gmail.com>
2017-10-07 21:49:38 +0200

    Formatted

--- src/iJoshuaHD/VIPSlots.php
+++ src/iJoshuaHD/VIPSlots.php
@@ -15,183 +15,173 @@ use pocketmine\utils\Config;
 
 class VIPSlots extends PluginBase implements Listener{
 
-    public function onEnable(){
+	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
-		
+
 		$this->loadVIPSList();
-		
+
 		$this->getLogger()->info("VIPSlots Enabled!");
 	}
-    
-    public function onDisable(){
+
+	public function onDisable(){
 		$this->getLogger()->info("VIPSlots Disabled!");
-    }
+	}
 
 	/*****************
-	*================*
-	*===[ Events ]===*
-	*================*
-	*****************/
+	 *================*
+	 *===[ Events ]===*
+	 *================*
+	 *****************/
 
 	public function onPlayerKick(PlayerKickEvent $event){
 		if($this->vips->exists(strtolower($event->getPlayer()->getName())) and $event->getReason() === "disconnectionScreen.serverFull"){
 			$event->setCancelled(true);
 		}
-		
+
 	}
 
 
 	/*****************
-	*================*
-	*==[ Commands ]==*
-	*================*
-	*****************/
-	
+	 *================*
+	 *==[ Commands ]==*
+	 *================*
+	 *****************/
+
 	public function onCommand(CommandSender $p, Command $command, string $label, array $args) : bool{
-	
+
 		if($command->getName() == "vips"){
-		
+
 			if(!isset($args[0]) || count($args) > 2){
 				$p->sendMessage("Usage: /vips <add/remove/list>");
 				return true;
 			}
-			
-			switch(strtolower($args[0]))
-			{
+
+			switch(strtolower($args[0])){
 				case "add":
-				
+
 					if(isset($args[1])){
 						$who_player = $this->getValidPlayer($args[1]);
-						
+
 						if($who_player instanceof Player){
 							$target = $who_player->getName();
-						}
-						else{
+						}else{
 							$target = $args[1];
 						}
-						
+
 						if($this->addPlayer($target)){
 							$p->sendMessage("Successfully added '$target' on VIPSlots!");
-						}
-						else{
+						}else{
 							$p->sendMessage("$target is already added on VIPSlots!");
 						}
-					}
-					else{
+					}else{
 						$p->sendMessage("Usage: /vips add <player>");
 					}
-				
+
 					break;
-					
+
 				case "remove":
-				
+
 					if(isset($args[1])){
 						$who_player = $this->getValidPlayer($args[1]);
-						
+
 						if($who_player instanceof Player){
 							$target = $who_player->getName();
-						}
-						else{
+						}else{
 							$target = $args[1];
 						}
-						
+
 						if($this->removePlayer($target)){
 							$p->sendMessage("Successfully removed '$target' on VIPSlots!");
-						}
-						else{
+						}else{
 							$p->sendMessage("$target doesn't exist on VIPSlots!");
 						}
-					}
-					else{
+					}else{
 						$p->sendMessage("Usage: /vips remove <player>");
 					}
-					
+
 					break;
-					
+
 				case "list":
-				
+
 					$file = fopen($this->getDataFolder() . "vip_players.txt", "r");
 					$i = 0;
-					while (!feof($file)) {
+					while(!feof($file)){
 						$vips[] = fgets($file);
 					}
 					fclose($file);
 
 					$p->sendMessage("-==[ VIPSlots List ]==-");
-					foreach ($vips as $vip){
+					foreach($vips as $vip){
 						$p->sendMessage(" - " . $vip);
 					}
-				
+
 					break;
-					
+
 				default:
-				
+
 					$p->sendMessage("Usage: /vips <add/remove/list>");
-					
+
 					break;
-					
+
 			}
 		}
 		return true;
 	}
-	
+
 	/*****************
-	*================*
-	*==[ Non-APIs ]==*
-	*================*
-	*****************/
-	
+	 *================*
+	 *==[ Non-APIs ]==*
+	 *================*
+	 *****************/
+
 	private function loadVIPSList(){
 		@mkdir($this->getDataFolder(), 0777, true);
-		$this->vips = new Config($this->getDataFolder() . "vip_players.txt", Config::ENUM, array(
-		));
+		$this->vips = new Config($this->getDataFolder() . "vip_players.txt", Config::ENUM, array());
 	}
-	
+
 	private function getValidPlayer($target){
 		$player = $this->getServer()->getPlayer($target);
 		return $player instanceof Player ? $player : $this->getServer()->getOfflinePlayer($target);
 	}
-	
+
 	/*****************
-	*================*
-	*==[   APIs   ]==*
-	*================*
-	*****************/
-	
+	 *================*
+	 *==[   APIs   ]==*
+	 *================*
+	 *****************/
+
 	public function addPlayer($player){
 		$target = $this->getValidPlayer($player);
-		
+
 		if($target instanceof Player){
 			$p = strtolower($target->getName());
-		}
-		else{
+		}else{
 			$p = strtolower($player);
 		}
-		
+
 		if($this->vips->exists($p)) return false;
-		
+
 		$this->vips->set($p, true);
 		$this->vips->save();
-			
+
 		return true;
 	}
-	
+
 	public function removePlayer($player){
-	
+
 		$target = $this->getValidPlayer($player);
-		
+
 		if($target instanceof Player){
 			$p = strtolower($target->getName());
-		}
-		else{
+		}else{
 			$p = strtolower($player);
 		}
-	
+
 		if(!$this->vips->exists($p)) return false;
-		
+
 		$this->vips->remove($p);
 		$this->vips->save();
-		
+
 		return true;
 	}
 }
